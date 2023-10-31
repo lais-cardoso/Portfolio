8d54e999dd2dc8610bee7e99e5bb4c25452c2f78
lais-cardoso <lalaicardoso27@hotmail.com>
2023-10-31 17:12:33 -0300

    feat: Create the resume page

---
+++ src/curriculum/index.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { LinkHeader, ContainerHeader, Image, Title, Paragraph, ContainerSection, ContainerBody, ContainerMain, Marks } from './styles'
+import ufrbLogo from "../assets/ufrb-logo.jpg"
+import pesquisaLogo from "../assets/pesquisa-logo.jpeg"
+import premiacaoLogo from "../assets/premiacao-logo.jpeg"
+import tecnologias from "../assets/tecnologias.png"
+import eventos from "../assets/eventos.jpeg"
+
+function Curriculum() {
+    const navigate = useNavigate();
+
+    function toHome() {
+        return navigate("/")
+    }
+
+    function toCurriculum() {
+        return navigate("/curriculum")
+    }
+
+    function toProjects() {
+        return navigate("/projects")
+    }
+    return (
+        <>
+            <ContainerHeader>
+                <LinkHeader onClick={toHome}>Sobre mim</LinkHeader>
+                <LinkHeader onClick={toCurriculum}>Currículo</LinkHeader>
+                <LinkHeader onClick={toProjects}>Projetos</LinkHeader>
+            </ContainerHeader>
+
+            <ContainerBody>
+                <ContainerMain>
+                    <ContainerSection>
+                        <Title>Formação</Title>
+
+                        <Marks>Bacharela em Ciências Exatas e Tecnológicas - Universidade Federal do Recôncavo da Bahia (2019 - 2023)</Marks>
+                        <Marks>Bacharelanda em Engenharia da Computação - Universidade Federal do Recôncavo da Bahia (2023 - Atual)</Marks>
+
+
+                    </ContainerSection>
+                    <ContainerSection>
+                        <Image src={ufrbLogo} />
+                    </ContainerSection>
+                </ContainerMain>
+
+                <ContainerMain>
+                    <ContainerSection>
+                        <Image src={tecnologias} />
+                    </ContainerSection>
+                    <ContainerSection>
+                        <Title>Tecnologias</Title>
+                        <Paragraph>Atualmente estou me aperfeiçoando nas seguintes tecnologias: HTML, CSS, Material UI Design, React.js, Node.js, Express, SQL, PostgreSQL, Git, Java, R, Python, C, thunderClient, PostBird, Insomnia, MongoDBCompass, MongoDB, Docker. </Paragraph>
+                    </ContainerSection>
+
+                </ContainerMain>
+
+                <ContainerMain>
+                    <ContainerSection>
+                        <Title>Pesquisa</Title>
+                        <Marks>Bolsista - Projeto de Iniciação Científica (10/2022 - 10/2023) </Marks>
+                        <Marks>Bolsista - Projeto de Extensão (05/2022 - 12/2022)  </Marks>
+                        <Marks>Bolsista - Projeto de Extensão (05/2021 - 12/2021) </Marks>
+                    </ContainerSection>
+                    <ContainerSection>
+                        <Image src={pesquisaLogo} />
+                    </ContainerSection>
+                </ContainerMain>
+
+                <ContainerMain>
+                    <ContainerSection>
+                        <Image src={premiacaoLogo} />
+                    </ContainerSection>
+                    <ContainerSection>
+                        <Title>Premiações</Title>
+                        <Marks>Melhor Artigo na Área Neural and Machine Learning System do XVI Congresso Brasileiro de Inteligência Computacional.  </Marks>
+                        <Marks>4º lugar na Competição Brasileira de Robótica na Categoria
+                            De Simulação 2D (2023)  </Marks>
+                        <Marks>4º lugar na Robocori na Categoria De Simulação 2D (2022) </Marks>
+                        <Marks>4º lugar na Competição Brasileira de Robótica na Categoria De Simulação 2D (2021) </Marks>
+                    </ContainerSection>
+                </ContainerMain>
+
+                <ContainerMain>
+                    <ContainerSection>
+                        <Title>Eventos</Title>
+                        <Marks>Competição Brasileira de Robótica (Salvador/2023) </Marks>
+                        <Marks>Congresso Brasileiro de Inteligência Computacional (Salvador/2023) </Marks>
+                        <Marks>Mostra Nacional de Robótica (Salvador/2023) </Marks>
+                        <Marks>IX Reconcitec (Cruz das Almas/2023) </Marks>
+                    </ContainerSection>
+                    <ContainerSection>
+                        <Image src={eventos} />
+                    </ContainerSection>
+                </ContainerMain>
+
+            </ContainerBody>
+
+        </>
+    )
+}
+
+export default Curriculum;
---
+++ src/curriculum/styles.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const LinkHeader = styled.a`
+    cursor: pointer;
+    margin-right: 32px;
+    color: #17233D;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: 0.875px;
+`
+
+export const ContainerHeader = styled.div`
+    margin-top: 47px;
+    margin-left: 42px;
+`
+
+export const Image = styled.img`
+    width: 350px;
+    height: 250px;
+`
+
+export const Title = styled.h1`
+    font-size: 40px;
+    font-weight: 700;
+    margin-bottom: 5%;
+`
+
+export const Paragraph = styled.p`
+    width: 624px;
+    height: 142px;
+    font-size: 16px;
+    font-weight: 400;
+`
+
+export const Marks = styled.li`
+    width: 624px;
+    font-size: 16px;
+    font-weight: 400;
+`
+
+export const ContainerBody = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 5%;
+`
+export const ContainerMain = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 5%;
+`
+
+export const ContainerSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 85px;
+`
