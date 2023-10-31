3613d053cea89fced043bf33b1fed1c766b12b36
lais-cardoso <lalaicardoso27@hotmail.com>
2023-10-31 17:42:25 -0300

    feat: creat notFound Page

---
+++ src/notFound/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Title, ContainerBody } from './styles'
+
+function NotFound() {
+    return (
+        <ContainerBody>
+            <Title>Not Found 404: Página Inexistente.</Title>
+        </ContainerBody>
+    )
+}
+
+export default NotFound;
---
+++ src/notFound/styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Title =  styled.h1`
+    color: #161A49;
+    width: 582px;
+    font-size: 62px;
+    margin: 10% 0 0 0;
+    font-weight: 700;
+    line-height: 82px;
+    letter-spacing: 0.596px;
+    text-align: center;
+`
+
+export const ContainerBody = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5%;
+`
