b848c24dff1f3aa474f747a676667cebea30d2ae
lais-cardoso <lalaicardoso27@hotmail.com>
2023-10-31 17:41:54 -0300

    feat: add notFound route

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,14 +4,17 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../src/home';
 import Curriculum from './curriculum'
 import Projects from '../src/projects'
+import NotFound from '../src/notFound'
 
 function Root(){
     return(
         <Router>
             <Routes>
                 <Route path="/" element={<Home />}/>
+                <Route path="/home" element={<Home />}/>
                 <Route path="/curriculum" element={<Curriculum />}/>
                 <Route path="/projects" element={<Projects />}/>
+                <Route element={<NotFound/>} path="*" />
             </Routes>
         </Router>
     )
